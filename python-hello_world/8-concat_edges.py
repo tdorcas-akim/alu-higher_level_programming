311b3cd8ed6454c3ff1df1b606f43d22d44d1f33
Dorcas <tdorcas-akim>
2025-02-15 06:20:39 -0800

    create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-str1 = "object"
-str2 = "oriented"
-str3 = "programming"
-str4 = "with"
-str5 = "Python"
-print(f"{str1}-{str2} {str3} {str4} {str5}")
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:67] + " with Python")
