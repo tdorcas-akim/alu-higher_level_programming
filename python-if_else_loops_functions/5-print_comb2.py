bfa69e3833742092f3bac556b011309845f16273
Dorcas <tdorcas-akim>
2025-02-15 11:47:25 -0800

    00  99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    if i != 98:
-        print("{:02d}".format(i), end=", ")
+for i in range(0, 100):
+    if i == 99:
+        print("{}".format(i))
     else:
-        print("{:02d}".format(i))
+        print("{:02d}, ".format(i), end='')
