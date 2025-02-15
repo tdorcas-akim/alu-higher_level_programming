fb42caa921be3a51baf94c3ce102f533ebc0e9ad
Dorcas <tdorcas-akim>
2025-02-15 11:41:23 -0800

    00 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 99):
-    print("{:02d}".format(i), end=", " if i < 98 else "\n")
+    if i != 98:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:02d}".format(i))
