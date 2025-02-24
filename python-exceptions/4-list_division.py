97385dc92ca4b975526903afe8bab3a68022e5aa
Dorcas <tdorcas-akim>
2025-02-24 11:58:46 -0800

    divide a list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
@@ -17,4 +16,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             result.append(0)
+        finally:
+            pass
     return result
