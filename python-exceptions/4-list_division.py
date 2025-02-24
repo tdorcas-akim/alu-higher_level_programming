2c18689b9ea092299e7086dc0388543a0e6e11b9
Dorcas <tdorcas-akim>
2025-02-24 11:53:39 -0800

    divide a list

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError
+            if type(my_list_1[i]) not in [int, float] or type(my_list_2[i]) not in [int, float]:
+                print("wrong type")
+                result.append(0)
+            else:
+                result.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+    return result
