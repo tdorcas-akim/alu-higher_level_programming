fb77e8f4f14182fe75322660cb64894f599c068b
Dorcas <tdorcas-akim>
2025-02-25 12:06:58 -0800

    area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+It defines a Square class with a private size attribute.
+It validates size type and value, and calculates area.
+"""
+
+
+class Square:
+    """This class defines a square with a private size attribute."""
+    def __init__(self,size=0):
