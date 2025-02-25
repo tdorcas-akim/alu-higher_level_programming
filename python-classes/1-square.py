915846888b3d72d7cad5726226e5d00835d17eb4
Dorcas <tdorcas-akim>
2025-02-25 11:22:32 -0800

    square with private size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square with a private size attribute.
+"""
+
+
+class Square:
+    """This class defines a square with a private size attribute."""
+
+    def__init__(self, size):
+        """Initializes square with a private size attribute."""
+        self.__size = size
