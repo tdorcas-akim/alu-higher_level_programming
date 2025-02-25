e72ce6deff3be8b2f6cf5078aa57a5733d8b448d
Dorcas <tdorcas-akim>
2025-02-25 13:32:25 -0800

    Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,7 +7,8 @@ position.
 
 
 class Square:
-    """This class defines a square with private size and position attributes."""
+    """This class defines a square with private size and position 
+        attributes."""
 
     def __init__(self, size=0, position=(0, 0)):
         """
