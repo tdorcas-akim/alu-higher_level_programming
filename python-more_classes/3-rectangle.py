70d961168be63a4416267e8c2382641dc0adc130
Dorcas <tdorcas-akim>
2025-03-03 11:56:05 -0800

    string

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -84,7 +84,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-    
+
     def __str__(self):
         """ Method that returns the Rectangle #
 
@@ -96,7 +96,7 @@ class Rectangle:
 
         if self.width == 0 or self.height == 0:
             return rectangle
-        
+
         for i in range(self.height):
             rectangle += ("#" * self.width) + "\n"
 
