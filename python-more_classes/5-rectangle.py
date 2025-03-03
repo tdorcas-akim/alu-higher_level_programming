4be5c07a53db0c789b05df674b0e022720328a99
Dorcas <tdorcas-akim>
2025-03-03 12:48:32 -0800

    instance deletion

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -93,11 +93,11 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        
+
         rectangle_str = []
         for i in range(self.__height):
             rectangle_str.append("#" * self.__width)
-        
+
         return "\n".join(rectangle_str)
 
     def __repr__(self):
