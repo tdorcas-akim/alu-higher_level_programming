eb528777467f694815d84ae13461dcf47eb441e9
Dorcas <tdorcas-akim>
2025-03-03 09:58:21 -0800

    real definition of a rectangle

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module containing Rectangle class definition
+"""
+
+
+def height(self):
+    """ method that returns the value of the height
+
+    Returns:
+        height of the rectangle
+
+
+    """
+    
+    return self.__height
+
+def height(self, value):
+    """ method that defines the height
+
+    Args:
+        value: height
+
+    Raises:
+        TypeError: if height is not an integer
+        ValueError: if height is less than zero
+
+
+        """
+        
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
