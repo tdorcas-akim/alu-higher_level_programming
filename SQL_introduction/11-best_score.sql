412f3024704d473abd569943ee335b0c8b0214c9
Dorcas <tdorcas-akim>
2025-03-13 11:07:44 -0700

    best score

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
 -- This script lists all records from the second_table in the hbtn_0c_0 database
 -- with a score greater than or equal to 10. The results display both the scor
---e and the name, ordered by score (top score first).
+-- e and the name, ordered by score (top score first).
 
 SELECT score, name
 FROM second_table
