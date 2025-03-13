0078c1abe61096cc41eeec418913955aa25cc597
Dorcas <tdorcas-akim>
2025-03-13 06:09:57 -0700

    10-student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This module defines a Student class with attributes and a method to retrieve a
-dictionary representationof the student with optional attribute filtering.
+dictionary representation.
 """
 
 
@@ -9,15 +9,13 @@ class Student:
     """Class representing a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a student with first_name, last_name, and age."""
+        """Initializes the student with the given name and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """Returns a dictionary representation of the student, filtering by at
-        trs if provided."""
+    def to_json(self):
+        """Returns a dictionary representation of the student."""
         if attrs is None:
-            return self.__dict__
-        return {key: value for key, value in self.__dict__.items() if key in a
-                ttrs}
+            return self.__dict_
+        return {key: value for key, value in self.__dict__.items() if key in attrs}
