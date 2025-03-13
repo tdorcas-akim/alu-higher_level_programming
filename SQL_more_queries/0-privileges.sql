0bbaae5046dc09153599bb42775f83efe53e317d
Dorcas <tdorcas-akim>
2025-03-13 12:10:50 -0700

    privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -3,5 +3,5 @@
 -- List privileges for user_0d_1 (the root user)
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- List privileges for user_0d_2 on the user_2_db database
-SHOW GRANTS FOR 'user_0d_2'@'localhost' LIKE 'GRANT SELECT, INSERT% ON `user_2_db`.* TO `%`';
+-- List privileges for user_0d_2 on the specific user_2_db database
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
