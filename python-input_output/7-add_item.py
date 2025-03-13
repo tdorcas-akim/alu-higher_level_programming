292f0cbc7bb8c2a94bcb8d05ceda73f33adde9a9
Dorcas <tdorcas-akim>
2025-03-13 05:39:03 -0700

    load, save, add

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,6 @@ Script that adds all arguments to a Python list, then saves them to a file.
 
 
 import sys
-
-
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
