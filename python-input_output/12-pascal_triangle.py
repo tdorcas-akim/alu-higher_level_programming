f5e3993ada0b37767776405f03b99856c4456b44
Dorcas <tdorcas-akim>
2025-03-13 06:34:00 -0700

    pascal's triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module generates Pascal's triangle.
+"""
+
+
+def pascal_triangle(n):
+    """Return Pascal's triangle up to the nth row."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
