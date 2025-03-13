60b6efc9b742255ea4c4a30a1f444d7de1e0bc79
Dorcas <tdorcas-akim>
2025-03-13 05:30:37 -0700

    load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that adds all arguments to a Python list, then saves them to a file.
+"""
+
+
+import sys
+
+
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    items_list = load_from_json_file(filename)
+except FileNotFoundError:
+    items_list = []
+
+# Add arguments (excluding the script name) to the list
+items_list.extend(sys.argv[1:])
+
+# Save the updated list to the file
+save_to_json_file(items_list, filename)
