bedae9a68763c49140d71675358983675d567df5
Dorcas <tdorcas-akim>
2025-03-13 12:51:49 -0700

    root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,7 +2,8 @@
 -- It grants the user all privileges and sets the password to 'user_0d_1_pwd'
 
 -- Check if the user 'user_0d_1' already exists and create the user if not
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 
+'user_0d_1_pwd';
 
 -- Grant all privileges to the user 'user_0d_1' on all databases
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
