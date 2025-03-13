f96d7516534c03de9ad81065fe3dcd5c4ed0d750
Dorcas <tdorcas-akim>
2025-03-13 11:17:29 -0700

    average

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- This script computes the average score of all records in the second_table
+-- of the hbtn_0c_0 database. The result is displayed in a column named 'avera
+-- ge'.
+
+SELECT AVG(score) AS average
+FROM second_table;
