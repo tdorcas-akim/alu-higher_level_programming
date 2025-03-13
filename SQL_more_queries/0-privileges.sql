90349d5a4f353478448fe13a8adfe40b9be51093
Dorcas <tdorcas-akim>
2025-03-13 12:06:43 -0700

    privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,8 +1,7 @@
--- This script lists all privileges for the MySQL users 'user_0d_1' and 'user_
--- 0d_2' on the MySQL server.
+-- This script lists all privileges for the MySQL users 'user_0d_1' and 'user_0d_2' on the MySQL server.
 
--- List privileges for user_0d_1
+-- List privileges for user_0d_1 (the root user)
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- List privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- List privileges for user_0d_2 on the user_2_db database
+SHOW GRANTS FOR 'user_0d_2'@'localhost' LIKE 'GRANT SELECT, INSERT% ON `user_2_db`.* TO `%`';
