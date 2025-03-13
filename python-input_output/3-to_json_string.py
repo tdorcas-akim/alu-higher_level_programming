b35a3775db0ce59c81362142f7a9ae404ac5f02d
Dorcas <tdorcas-akim>
2025-03-13 04:30:20 -0700

    JSON

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module to return the JSON representation of an object."""
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)."""
+    return str(my_obj)
