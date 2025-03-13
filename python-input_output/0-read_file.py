6e86330de23a3966a99b60cbba02e11768839dad
Dorcas <tdorcas-akim>
2025-03-13 02:53:39 -0700

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints its content to stdout."""
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end="")
