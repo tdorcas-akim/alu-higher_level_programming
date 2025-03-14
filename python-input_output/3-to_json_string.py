a9e24d652de353951a389962954099ef1544edb2
Dorcas <tdorcas-akim>
2025-03-14 11:01:06 -0700

    to json string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,27 +1,8 @@
 #!/usr/bin/python3
-"""
-Module that provides a function to return the JSON representation of an obj.
-"""
-
-
+"""Returns the JSON representation of an object (string):."""
 import json
 
 
 def to_json_string(my_obj):
-    """
-    Returns the JSON representation of an object (string).
-    
-    Args:
-        my_obj (any type): The object to be converted to JSON string.
-    
-    Returns:
-        str: The JSON representation of the object as a string.
-    
-    Raises:
-        TypeError: If the object can't be serialized to JSON.
-    """
-    try:
-        return json.dumps(my_obj)
-    except TypeError:
-        raise TypeError(f"Object of type {type(my_obj).__name__} is not JSON s
-        erializable")
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
