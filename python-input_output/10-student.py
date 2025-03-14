536c448842db4c9eb4df81875441964b7bf1b06b
Dorcas <tdorcas-akim>
2025-03-14 11:05:51 -0700

    student to json

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""
-This module defines a Student class with a method to return a dictionary repre
-sentation of the student.
-"""
+"""Write a class Student that defines a student """
 
 
 class Student:
-    """Student class with first name, last name, and age attributes."""
-
+    """Representation of a student"""
     def __init__(self, first_name, last_name, age):
-        """Initialize student with first name, last name, and age."""
+        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return a dictionary representation of the student, filtered by attr
-        s."""
-        if attrs is None:
-            return self.__dict__
-        return {key: value for key, value in self.__dict__.items() if key in a
-                ttrs}
+        """returns a dictionary representation of a Student instance"""
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {y: getattr(self, y) for y in attrs if hasattr(self, y)}
+        return self.__dict__
