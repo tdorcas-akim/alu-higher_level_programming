65f488056621debf28b1a7888180b9fc40b72b6f
Dorcas <tdorcas-akim>
2025-04-01 14:47:40 -0700

    header & post_params

---
+++ python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends a GET request to the URL with a custom header X-HolbertonSchool-User-Id: 98 and displays the body of the response
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
