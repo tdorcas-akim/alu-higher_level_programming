1d6d18dd83fe0ed9c88910c567761b2eefd8adca
Dorcas <tdorcas-akim>
2025-04-02 05:50:49 -0700

    Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,9 +7,7 @@ This function adds 2 integers
 def add_integer(a, b=98):
     """ Adds two integers or floats. If any input is a float, it is casted int
     o an integer. """
-
-
-if type(a) not in [int, float]:
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
