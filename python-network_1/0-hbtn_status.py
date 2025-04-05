e5c5dd0c1db9237c643f489ff8b23354b2392d17
Dorcas <tdorcas-akim>
2025-04-05 08:59:25 -0700

    what's my status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
-A script that fetches https://alu-intranet.hbtn.io/status
-using the urllib package and displays the response in a
-specific format.
-"""
+'''a Python script that fetches a url'''
 
 
 import urllib.request
 
-if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body}")
-        print(f"\t- utf8 content: {body.decode('utf-8')}")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
