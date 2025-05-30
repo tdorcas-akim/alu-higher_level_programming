127c339aeef24fc5807c221e10cd70a6b0845487
Dorcas <tdorcas-akim>
2025-05-30 13:56:31 -0700

    Changes made Square & Square1

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+
 const SquareParent = require('./5-square');
 
 class Square extends SquareParent {
-  charPrint(c) {
+  charPrint (c) {
     const char = c === undefined ? 'X' : c;
 
     for (let i = 0; i < this.height; i++) {
